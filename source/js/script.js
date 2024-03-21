5368d977fa09b2e50d4c1fa2a758a2637229b2ef
Ilichov Denis <d.ili4ov@gmail.com>
2024-03-21 17:03:34 +0300

    Добавил состояния и локальное хранилище

--- source/js/script.js
+++ source/js/script.js
@@ -1,118 +1,196 @@
-const state = new Proxy({ // Объявляем прокси-объект и помещаем в него массив с названием, описанием и айди задачи
-    tasks: [
-        { id: 1, title: "Приготовить обед", description: "Было бы неплохо приготовить обед сегодня утром." }
-    ],
-    selectedTaskIds: []
-}, {
-    set: function (target, property, value) { // Создаём прокси-объект
+const state = new Proxy({
+    tasks: [],
+    selectedTaskIds: [],
+    editingTaskId: null,
+    viewState: 'view',
+    deleteState: false
+  }, {
+    set: function(target, property, value) {
         target[property] = value;
-        renderTasks(); // Обновляем список задач
+        updateTasks();
+        saveToLocalStorage();
         return true;
     }
-});
-
-function createTask() { // Функция создания задачи по клику на кнопку
+  });
+  
+  function viewTasks() {
+    document.getElementById('viewTasksButton').style.display = "none";
+    document.getElementById('createEditTaskButton').style.display = "block";
+    document.getElementById('deleteTasksButton').style.display = "block";
+    document.getElementById('deleteSelectedButton').style.display = "none";
+    state.viewState = 'view';
+    state.deleteState = false;
+  }
+  
+  function createEditTask() {
+    document.getElementById('createEditTaskButton').style.display = "none";
+    document.getElementById('viewTasksButton').style.display = "block";
+    state.viewState = 'edit';
+    state.deleteState = false;
+  }
+  
+  function deleteTasks() {
+    state.deleteState = true;
+    state.viewState = 'delete';
+  }
+  
+  function createTask() {
     const title = document.getElementById("taskTitle").value;
     const description = document.getElementById("taskDescription").value;
-
+  
     if (title !== "") {
         const newTask = { id: Date.now(), title: title, description: description };
         state.tasks.push(newTask);
-        renderTasks(); // Обновляем список задач после добавления новой задачи
+        state.viewState = 'view';
+        viewTasks();
     } else {
         alert('Вы не ввели название задачи!');
     }
-
+  
     document.getElementById("taskTitle").value = "";
     document.getElementById("taskDescription").value = "";
-}
-
-function deleteTasks() { // Функция удаления задачи по клику на кнопку
+  }
+  
+  function deleteTask(taskId) {
+    state.tasks = state.tasks.filter(task => task.id !== taskId);
+  }
+  
+  function deleteSelectedTasks() {
     state.tasks = state.tasks.filter(task => !state.selectedTaskIds.includes(task.id));
     state.selectedTaskIds = [];
-    renderTasks(); // Обновляем список задач после удаления задач
-}
-
-function toggleTaskSelection(taskId) { // Функция переключения выбора задач
-    if (state.selectedTaskIds.includes(taskId)) {
-        state.selectedTaskIds = state.selectedTaskIds.filter(id => id !== taskId);
-    } else {
-        state.selectedTaskIds.push(taskId);
+  }
+  
+  function toggleTaskSelection(taskId) {
+    if (state.deleteState) {
+      if (state.selectedTaskIds.includes(taskId)) {
+          state.selectedTaskIds = state.selectedTaskIds.filter(id => id !== taskId);
+      } else {
+          state.selectedTaskIds.push(taskId);
+      }
     }
-}
-
-let editingTaskId = null;
-
-function selectTask(taskId) { // Функция управления редактированием задачи
-    editingTaskId = taskId;
-    renderTasks();
-}
-
-function saveEditedTask(newTitle, newDescription) { // Функция сохранения отредактированной задачи
-    const task = state.tasks.find(task => task.id === editingTaskId);
+  }
+  
+  function selectTask(taskId) {
+    state.editingTaskId = taskId;
+    state.viewState = 'edit';
+  }
+  
+  function saveEditedTask(newTitle, newDescription) {
+    const task = state.tasks.find(task => task.id === state.editingTaskId);
     if (task) {
         task.title = newTitle;
         task.description = newDescription;
-        editingTaskId = null;
-        renderTasks();
+        state.editingTaskId = null;
+        state.viewState = 'view';
     }
-}
-
-function renderTasks() { // Функция отображения списка задач на странице
+    viewTasks();
+  }
+  
+  function toggleCheckboxVisibility() {
+    const checkboxes = document.querySelectorAll('.task-checkbox');
+    checkboxes.forEach(checkbox => {
+      checkbox.style.display = state.deleteState ? 'block' : 'none';
+    });
+  }
+  
+  
+  function updateTasks() {
     const taskList = document.getElementById("taskList");
     taskList.innerHTML = "";
-
-    state.tasks.forEach(task => { // Создаём html элементы в списке
-        const li = document.createElement("li");
-
+  
+    state.tasks.forEach(task => {
+      const li = document.createElement("li");
+  
+      const title = document.createElement("strong");
+      title.textContent = task.title;
+  
+      const description = document.createElement("p");
+      description.textContent = task.description;
+  
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = 'Удалить';
+      deleteButton.addEventListener('click', () => deleteTask(task.id));
+  
+      const editButton = document.createElement("button");
+      editButton.textContent = 'Редактировать';
+      editButton.addEventListener('click', () => selectTask(task.id));
+  
+      if (state.deleteState) {
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
+        checkbox.className = "task-checkbox";
         checkbox.addEventListener('change', () => toggleTaskSelection(task.id));
         checkbox.checked = state.selectedTaskIds.includes(task.id);
-
-        const title = document.createElement("strong");
-        title.textContent = task.title;
-
-        const description = document.createElement("p");
-        description.textContent = task.description;
-
-        const editButton = document.createElement("button");
-        editButton.textContent = 'Редактировать';
-        editButton.addEventListener('click', () => selectTask(task.id));
-
         li.appendChild(checkbox);
-        li.appendChild(title);
-        li.appendChild(description);
-
-        if (task.id === editingTaskId) { // Условие сохранения задачи в режиме редактирования
-            const editForm = document.createElement("div");
-
-            const editTitleInput = document.createElement("input");
-            editTitleInput.type = "text";
-            editTitleInput.value = task.title;
-
-            const editDescriptionTextarea = document.createElement("textarea");
-            editDescriptionTextarea.value = task.description;
-
-            const saveButton = document.createElement("button");
-            saveButton.textContent = 'Сохранить';
-            saveButton.addEventListener('click', () => saveEditedTask(editTitleInput.value, editDescriptionTextarea.value));
-
-            editButton.style.display = "none"; // Скрыть кнопку "Редактировать"
-
-            editForm.appendChild(editTitleInput);
-            editForm.appendChild(editDescriptionTextarea);
-            editForm.appendChild(saveButton);
-
-            li.appendChild(editForm);
-        } else {
-            li.appendChild(editButton);
-        }
-
-        taskList.appendChild(li);
+      }
+  
+      li.appendChild(title);
+      li.appendChild(description);
+  
+      if (state.deleteState) {
+        li.appendChild(deleteButton);
+      }
+  
+      if (state.viewState === 'edit' && state.editingTaskId === task.id) {
+        const editForm = document.createElement("div");
+  
+        const editTitleInput = document.createElement("input");
+        editTitleInput.type = "text";
+        editTitleInput.value = task.title;
+  
+        const editDescriptionTextarea = document.createElement("textarea");
+        editDescriptionTextarea.value = task.description;
+  
+        const saveButton = document.createElement("button");
+        saveButton.textContent = 'Сохранить';
+        saveButton.addEventListener('click', () => saveEditedTask(editTitleInput.value, editDescriptionTextarea.value));
+  
+        editForm.appendChild(editTitleInput);
+        editForm.appendChild(editDescriptionTextarea);
+        editForm.appendChild(saveButton);
+  
+        li.appendChild(editForm);
+      } else if (state.viewState === 'edit') {
+        li.appendChild(editButton);
+      }
+  
+      taskList.appendChild(li);
     });
-}
-
-renderTasks();
-
-
+    toggleCheckboxVisibility();
+  }
+  function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(state.tasks));
+  }
+  
+  function loadFromLocalStorage() {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+        state.tasks = JSON.parse(savedTasks);
+    }
+  }
+  
+  document.getElementById('viewTasksButton').addEventListener('click', viewTasks);
+  document.getElementById('createEditTaskButton').addEventListener('click', createEditTask);
+  document.getElementById('deleteTasksButton').addEventListener('click', deleteTasks);
+  document.getElementById('deleteSelectedButton').addEventListener('click', () => {
+    deleteSelectedTasks();
+    state.deleteState = false;
+  });
+  
+  function showDeleteSelectedButton() {
+    document.getElementById('deleteTasksButton').style.display = 'none';
+    document.getElementById('deleteSelectedButton').style.display = 'block';
+  }
+  
+  function showDeleteTasksButton() {
+    document.getElementById('deleteTasksButton').style.display = 'block';
+    document.getElementById('deleteSelectedButton').style.display = 'none';
+  }
+  
+  document.getElementById('deleteTasksButton').addEventListener('click', showDeleteSelectedButton);
+  document.getElementById('deleteSelectedButton').addEventListener('click', showDeleteTasksButton);
+  
+  loadFromLocalStorage();
+  updateTasks();
+  
+  
